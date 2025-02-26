945a17e64e7e4397c6e16caa44d82b44f542796d
Alex <yoyobq@hotmail.com>
2025-02-26 13:27:04 +0800

    build: 由于 @umijs/lint 的限制，将 eslint 版本回退至 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,10 @@ module.exports = {
     page: true,
     REACT_APP_ENV: true,
   },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      // 你可以根据需要添加更多配置，例如设置规则等
+    },
+  ],
 };
