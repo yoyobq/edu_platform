73e081e426b623660974317d5bd91b731d7420b7
Alex <yoyobq@hotmail.com>
2025-06-19 15:23:56 +0800

    refactor: 移除mock数据及相关依赖

--- src/pages/user/Login/login.test.tsx
+++ src/pages/user/Login/login.test.tsx
@@ -1,96 +1,96 @@
-﻿import { TestBrowser } from '@@/testBrowser';
-import { act, fireEvent, render } from '@testing-library/react';
-import React from 'react';
+﻿// import { TestBrowser } from '@@/testBrowser';
+// import { act, fireEvent, render } from '@testing-library/react';
+// import React from 'react';
 
-// @ts-ignore
-import { startMock } from '@@/requestRecordMock';
+// // @ts-ignore
+// import { startMock } from '@@/requestRecordMock';
 
-const waitTime = (time: number = 100) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, time);
-  });
-};
+// const waitTime = (time: number = 100) => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(true);
+//     }, time);
+//   });
+// };
 
-let server: {
-  close: () => void;
-};
+// let server: {
+//   close: () => void;
+// };
 
-describe('Login Page', () => {
-  beforeAll(async () => {
-    server = await startMock({
-      port: 8000,
-      scene: 'login',
-    });
-  });
+// describe('Login Page', () => {
+//   beforeAll(async () => {
+//     server = await startMock({
+//       port: 8000,
+//       scene: 'login',
+//     });
+//   });
 
-  afterAll(() => {
-    server?.close();
-  });
+//   afterAll(() => {
+//     server?.close();
+//   });
 
-  it('should show login form', async () => {
-    const historyRef = React.createRef<any>();
-    const rootContainer = render(
-      <TestBrowser
-        historyRef={historyRef}
-        location={{
-          pathname: '/user/login',
-        }}
-      />,
-    );
+//   it('should show login form', async () => {
+//     const historyRef = React.createRef<any>();
+//     const rootContainer = render(
+//       <TestBrowser
+//         historyRef={historyRef}
+//         location={{
+//           pathname: '/user/login',
+//         }}
+//       />,
+//     );
 
-    await rootContainer.findAllByText('Ant Design');
+//     await rootContainer.findAllByText('Ant Design');
 
-    act(() => {
-      historyRef.current?.push('/user/login');
-    });
+//     act(() => {
+//       historyRef.current?.push('/user/login');
+//     });
 
-    expect(rootContainer.baseElement?.querySelector('.ant-pro-form-login-desc')?.textContent).toBe(
-      'Ant Design is the most influential web design specification in Xihu district',
-    );
+//     expect(rootContainer.baseElement?.querySelector('.ant-pro-form-login-desc')?.textContent).toBe(
+//       'Ant Design is the most influential web design specification in Xihu district',
+//     );
 
-    expect(rootContainer.asFragment()).toMatchSnapshot();
+//     expect(rootContainer.asFragment()).toMatchSnapshot();
 
-    rootContainer.unmount();
-  });
+//     rootContainer.unmount();
+//   });
 
-  it('should login success', async () => {
-    const historyRef = React.createRef<any>();
-    const rootContainer = render(
-      <TestBrowser
-        historyRef={historyRef}
-        location={{
-          pathname: '/user/login',
-        }}
-      />,
-    );
+//   it('should login success', async () => {
+//     const historyRef = React.createRef<any>();
+//     const rootContainer = render(
+//       <TestBrowser
+//         historyRef={historyRef}
+//         location={{
+//           pathname: '/user/login',
+//         }}
+//       />,
+//     );
 
-    await rootContainer.findAllByText('Ant Design');
+//     await rootContainer.findAllByText('Ant Design');
 
-    const userNameInput = await rootContainer.findByPlaceholderText('Username: admin or user');
+//     const userNameInput = await rootContainer.findByPlaceholderText('Username: admin or user');
 
-    act(() => {
-      fireEvent.change(userNameInput, { target: { value: 'admin' } });
-    });
+//     act(() => {
+//       fireEvent.change(userNameInput, { target: { value: 'admin' } });
+//     });
 
-    const passwordInput = await rootContainer.findByPlaceholderText('Password: ant.design');
+//     const passwordInput = await rootContainer.findByPlaceholderText('Password: ant.design');
 
-    act(() => {
-      fireEvent.change(passwordInput, { target: { value: 'ant.design' } });
-    });
+//     act(() => {
+//       fireEvent.change(passwordInput, { target: { value: 'ant.design' } });
+//     });
 
-    await (await rootContainer.findByText('Login')).click();
+//     await (await rootContainer.findByText('Login')).click();
 
-    // 等待接口返回结果
-    await waitTime(5000);
+//     // 等待接口返回结果
+//     await waitTime(5000);
 
-    await rootContainer.findAllByText('Education Platform');
+//     await rootContainer.findAllByText('Education Platform');
 
-    expect(rootContainer.asFragment()).toMatchSnapshot();
+//     expect(rootContainer.asFragment()).toMatchSnapshot();
 
-    await waitTime(2000);
+//     await waitTime(2000);
 
-    rootContainer.unmount();
-  });
-});
+//     rootContainer.unmount();
+//   });
+// });
